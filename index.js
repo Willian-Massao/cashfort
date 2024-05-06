530b6bcb2f745d319c9a21af1428dea3529521be
Willian <brcraftgames@gmail.com>
2024-05-06 20:48:13 -0300

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
