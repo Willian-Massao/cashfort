34ae89e5f9dba320699f8cbe8d559a096c2c0569
Willian <brcraftgames@gmail.com>
2024-05-07 11:59:35 -0300

    iniciado a pagina de produtos

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
+app.get('/products', (req, res) => {
+    res.render('product');
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
