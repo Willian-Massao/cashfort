8dea02eb4c6d619c43738d790e24330799671053
Willian <brcraftgames@gmail.com>
2024-06-10 20:06:44 -0300

    email sender

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express');
+const mailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const app = express();
 const itens = require('./itens.json');
 // mapear todas as marcas que ah no arquivo itens.json mas não duplicar ignorando letras maiusculas
 let brand = itens.map((item) => item.marca).filter((value, index, self) => self.indexOf(value) === index);
 brand = brand.sort();
 
+const smtp = mailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secureConnection: true,
+    auth: {
+        user: 'cashfortcontato@gmail.com',
+        pass: ''
+    }
+});
+
 console.log(brand);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 3000;
+
+app.post('/send', (req, res)=>{
+    const {name, email, text, target} = req.body;
+    console.log(target);
+    smtp.sendMail({
+        to: target,
+        subject: 'Contato CashFort',
+        html: `<h2>Nome: ${name}</h2><h2>Email: ${email}</h2><h3>Mensagem: ${text}</h3>`
+    }).then(()=>{
+        console.log('Mensagem enviada com sucesso!');
+    }).catch((error)=>{
+        console.log('Erro ao enviar mensagem: ' + error);
+    }).finally(()=>{
+        res.redirect('/contact');
+    });
+})
 
 app.get('/', (req, res) => {
     res.render('index', {marcas: brand});
